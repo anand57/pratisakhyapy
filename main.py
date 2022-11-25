103a6593064005cdfc4f86a4a312c1c6d4523e14
Anand Vemuri <anand@mytwinfire.com>
2022-11-25 10:47:31 -0800

    adding alphabet to unicode mapping

---
+++ main.py
@@ -0,0 +1,72 @@
+# 1-1 atha varnasamaamnaayh
+
+
+class Akshara:
+    def __init__(self, varna, swara):
+        self.varna = varna
+        self.swara = swara
+
+
+
+
+"""
+Represent the Vowels in Devanagari
+"""
+
+
+a = u'\u0905'
+A = u'\u0906'
+i = u'\u0907'
+I = u'\u0908'
+u = u'\u0909'
+U = u'\u090A'
+ru = u'\u090B'
+lru = u'\u090C'
+e = u'\u090F'
+ai = u'\u090E'
+
+
+"""
+Represent the Consonantrs in Devanagari
+"""
+
+k = u'\u0915'
+kh = u'\u0916'
+g = u'\u0917'
+gh = u'\u0918'
+nga = u'\u0919'
+
+c =  u'\u091A'
+ch = u'\u091B'
+j =  u'\u091C'
+jh =  u'\u091D'
+nya =  u'\u091E'
+T =  u'\u091F'
+Th =  u'\u0920'
+D =  u'\u0921'
+Dh =  u'\u0922'
+Na =  u'\u0923'
+t = u'\u0924'
+th = u'\u0925'
+d = u'\u0926'
+dh = u'\u0927'
+n = u'\u0928'
+p = u'\u092A'
+ph = u'\u092B'
+b = u'\u092C'
+bh = u'\u092D'
+m = u'\u092E'
+y = u'\u092F'
+r = u'\u0930'
+l = u'\u0932'
+L = u'\u0933'
+v = u'\u0935'
+sh = u'\u0936'
+Sh = u'\u0937'
+s = u'\u0938'
+h = u'\u0939'
+
+
+
+
+print(u'\u0922\u093E')
